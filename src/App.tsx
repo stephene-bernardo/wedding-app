d9092d9afd32275abb7fbdf41bcfbc4e8c72058b
Karl Bernardo <stephene.bernardo@gmail.com>
2024-01-30 21:25:38 +0800

    firebase hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 import Rsvp from './component/rsvp';
 import Divider from '@mui/material/Divider';
 
